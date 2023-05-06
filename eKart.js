f11a6e21704143f07f45bdf4cdb05147274cd2d3
Venkat-Chakravarthy <129709064+Venkat-Chakravarthy@users.noreply.github.com>
2023-05-05 22:37:03 -0500

    Update eKart.js

--- eKart.js
+++ eKart.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', []);
+ar app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope) {
     const userId = localStorage.getItem("userId");
     var URL = "https://fir-1c7de-default-rtdb.firebaseio.com";
